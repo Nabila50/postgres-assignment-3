a3bb1c38287261f7d2010d1eebf566ba4391677e
Nabila <nabila.nawshin50@gmail.com>
2026-06-28 00:51:58 +0200

    crate sql

--- query.sql
+++ query.sql
@@ -76,3 +76,48 @@ VALUES
 (8, 4, 3, '2023-12-05', '2023-12-06', 'Pending', 30),
 (9, 4, 2, '2023-12-15', '2023-12-18', 'Completed', 180),
 (10, 2, 4, '2023-12-20', '2023-12-22', 'Confirmed', 300)
+
+
+
+-- SQL Queries
+-- Query 1: JOIN
+select 
+b.booking_id,
+u.name AS customer_name,
+v.name AS vehicle_name,
+b.start_date,
+b.end_date,
+b.booking_status
+From Bookings b
+INNER JOIN users u
+on b.user_id = u.user_id
+INNER JOIN vehicles v
+ON b.vehicle_id = v.vehicle_id;
+
+-- Q_2: Exists
+
+SELECT *
+FROM Vehicles v
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Bookings b
+    WHERE b.vehicle_id = v.vehicle_id
+);
+
+-- Q_3: WHERE
+SELECT *
+FROM Vehicles
+WHERE type = 'car'
+AND status = 'available';
+
+-- Q-4: Group by and having
+SELECT
+    v.name AS vehicle_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM Vehicles v
+INNER JOIN Bookings b
+ON v.vehicle_id = b.vehicle_id
+GROUP BY
+    v.vehicle_id,
+    v.name
+HAVING COUNT(b.booking_id) > 2;
